0c531920072146bf0e72721a3d98cc04d536d599
Kalin Jack <kalinj1997@gmail.com>
2021-01-04 07:04:45 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,5 @@
 # Build your say_hello method here
 
-def say_hello ("Gabriela")
-  puts " Hello, #{Gabriela}"
-end
-
 def greeting
   puts "Hello, #{name}. We heard you are a great #{language} programmer."
 end
